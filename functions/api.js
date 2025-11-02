5bd3061e91d4ab3f81b00c1550b459bb7bef936b
Durgaa17 <sivaramhome86@gmail.com>
2025-11-02 09:30:31 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,4 @@
-// functions/api.js - Clean version
+// functions/api.js - With Basic Proxy Data
 exports.handler = async function(event, context) {
     const headers = {
         'Content-Type': 'application/json',
@@ -9,16 +9,31 @@ exports.handler = async function(event, context) {
         return { statusCode: 200, headers, body: '' };
     }
 
-    const { action } = event.queryStringParameters || {};
+    const { action, country } = event.queryStringParameters || {};
+
+    // Simple proxy data (we'll connect to data.js later)
+    const proxyData = {
+        sg: ['13.250.131.37:443', '54.255.185.94:443'],
+        my: ['52.77.225.242:443', '13.76.157.24:443'],
+        working: ['13.250.131.37:443', '54.255.185.94:443'],
+        lastUpdated: new Date().toISOString()
+    };
 
     if (action === 'proxies') {
+        let proxies = [];
+        if (!country || country === 'SG') proxies = proxies.concat(proxyData.sg);
+        if (!country || country === 'MY') proxies = proxies.concat(proxyData.my);
+        
         return {
             statusCode: 200,
             headers,
             body: JSON.stringify({
                 success: true,
-                message: 'Proxies endpoint working',
-                action: action
+                action: 'proxies',
+                country: country || 'all',
+                proxies: proxies,
+                count: proxies.length,
+                lastUpdated: proxyData.lastUpdated
             })
         };
     }
@@ -29,8 +44,46 @@ exports.handler = async function(event, context) {
             headers,
             body: JSON.stringify({
                 success: true,
-                message: 'Working proxies endpoint',
-                action: action
+                action: 'working',
+                proxies: proxyData.working,
+                count: proxyData.working.length,
+                lastUpdated: proxyData.lastUpdated
+            })
+        };
+    }
+
+    if (action === 'statistics') {
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({
+                success: true,
+                action: 'statistics',
+                data: {
+                    totalProxies: proxyData.sg.length + proxyData.my.length,
+                    workingProxies: proxyData.working.length,
+                    successRate: 75,
+                    lastUpdated: proxyData.lastUpdated
+                }
+            })
+        };
+    }
+
+    if (action === 'random') {
+        const pool = country === 'SG' ? proxyData.sg : 
+                    country === 'MY' ? proxyData.my : 
+                    [...proxyData.sg, ...proxyData.my];
+        
+        const randomProxy = pool[Math.floor(Math.random() * pool.length)];
+        
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({
+                success: true,
+                action: 'random',
+                country: country || 'all',
+                proxy: randomProxy
             })
         };
     }
@@ -40,8 +93,14 @@ exports.handler = async function(event, context) {
         headers,
         body: JSON.stringify({
             success: true,
-            message: 'API is working',
-            endpoints: ['proxies', 'working', 'statistics']
+            message: '🚀 SG/MY Proxy API',
+            version: '2.0',
+            endpoints: {
+                'proxies': 'Get proxies by country',
+                'working': 'Get working proxies', 
+                'statistics': 'Get statistics',
+                'random': 'Get random proxy'
+            }
         })
     };
 };
